f2074bbdc3ad9138ef570a890b983b901e44ed16
kseniiaprytkova <prytkovakseniia@gmail.com>
2018-02-26 20:11:14 +0200

    some fixes for min_width && min_height

--- ft_generate.c
+++ ft_generate.c
@@ -39,7 +39,8 @@ int 	min_sq_width(char **matrix)
 		}
 		row++;
 	}
-	if ( (matrix[0][0] == '.' && matrix[0][2] != '.') || (matrix[1][0] == '.' && matrix[1][1] != '.' && matrix[1][2] != '.'))
+	if ( (matrix[0][0] != '.' && matrix[0][1] != '.' && matrix[1][1] != '.' && matrix[1][2] != '.') ||
+	 	(matrix[0][1] != '.' && matrix[0][2] != '.' && matrix[1][0] != '.' && matrix[1][1] != '.'))
 		{
 			max_width++;
 		}
@@ -79,8 +80,8 @@ int 	min_sq_height(char **matrix)
 		printf("max: %i\n", max_height );
 		col++;
 	}
-		if ((matrix[0][0] == '.' && matrix[2][0] != '.') ||
-			(matrix[2][1] != '.' && matrix[0][0] != '.' && matrix[1][1] != '.'))
+		if ((matrix[0][1] != '.' && matrix[1][0] != '.' && matrix[1][1] != '.' && matrix[2][0] != '.') ||
+			(matrix[0][0] != '.' && matrix[1][0] != '.' && matrix[1][1] != '.' && matrix[2][1] != '.'))
 		{
 			max_height++;
 		}
