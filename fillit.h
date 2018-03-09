4194e5d8a71d35c7d252f86fadf5feb60fe45fb6
Kseniia <prytkovakseniia@gmail.com>
2018-03-09 17:02:20 +0200

    Update fillit.h

--- fillit.h
+++ fillit.h
@@ -22,39 +22,32 @@
 typedef struct		shape
 {
 	char			**matrix;
-	int				x;
-	int				y;
-}					figure;
+	int			x;
+	int			y;
+}				figure;
 
 char				*read_from_file(char *filename);
-int					ft_checktabl(char *str);
-int					min_sq_width(char **matrix);
-int					min_sq_height(char **matrix);
+int				ft_checktabl(char *str);
+int				min_sq_width(char **matrix);
+int				min_sq_height(char **matrix);
 char				**ft_generate(int squad_size);
 void				print_squard(char **squard, int squard_size);
-int					get_x_offset(char **matrix);
-int					get_y_offset(char **matrix);
+int				get_x_offset(char **matrix);
+int				get_y_offset(char **matrix);
 void				clean_matrix(char **matrix);
-void				move_shape(char **matrix, int get_x_offset,
-						int get_y_offset);
+void				move_shape(char **matrix, int get_x_offset, int get_y_offset);
 void				figure_offset(char **matrix);
-int					is_fit(char	**matrix, char **squard_to_fill, int x, int y);
-int					multiple_fit(char **squard_to_fill, figure *f,
-						int fig_counter, int squard_size);
-void				del_me(char **squard_to_fill, int nb_of_fig_to_del,
-						int squard_size);
-void				ok_here(char **squard_to_fill, figure *f,
-						int y, int x, int fig_counter);
-int					shift_me(char **squard_to_fill, figure *f, int fig_counter);
-int					should_extend(int *states, int index);
+int				is_fit(char	**matrix, char **squard_to_fill, int x, int y);
+int				multiple_fit(char **squard_to_fill, figure *f, int fig_counter, int squard_size);
+void				del_me(char **squard_to_fill, int nb_of_fig_to_del, int squard_size);
+void				ok_here(char **squard_to_fill, figure *f, int y, int x, int fig_counter);
+int				shift_me(char **squard_to_fill, figure *f, int fig_counter);
+int				should_extend(int *states, int index);
 void				free_me(char **squard_to_free, int	squard_size);
-int					where_is_xy(char **squard_to_fill, figure *f,
-						int fig_counter, int squard_size);
-int					can_i_shift(char **squard_to_fill, figure *f,
-						int fig_counter, int squard_size);
-int					control_fn(int fig_number, char **squard_to_fill,
-						int squard_size, figure **array_of_figures);
-int					is_full(char **squard_to_fill, int squard_size);
+int				where_is_xy(char **squard_to_fill, figure *f, int fig_counter, int squard_size);
+int				can_i_shift(char **squard_to_fill, figure *f, int fig_counter, int squard_size);
+int				control_fn(int fig_number, char **squard_to_fill, int squard_size, figure **array_of_figures);
+int				is_full(char **squard_to_fill, int squard_size);
 
 #endif
 
